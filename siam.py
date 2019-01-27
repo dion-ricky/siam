03c1453917c9b455fd77165fc189cacfbf2b557c
Dion Ricky Saputra <dionrickysptr@gmail.com>
2019-01-27 12:04:05 +0700

    SIAM Secure
    Implement CLI login, console login, safe login failure handler, and invisible password input

--- siam.py
+++ siam.py
@@ -2,21 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 from requests.exceptions import RequestException
 from get import get_data as get
+from getpass import getpass
 
 session = requests.Session()
+siam_home = "https://siam.ub.ac.id/"
 
-def login(username, pswd):
+def login(username=None, pswd = None, raw=True):
+    username = input("Username: ") if username == None else username # implement CLI login
+    pswd = getpass(prompt="Password: ") if pswd == None else pswd # input will not be visible
     try:
         parameters = {'username':username, 'password':pswd, 'login':'Masuk'}
 
-        with session.post("https://siam.ub.ac.id", data=parameters) as r:
+        with session.post(siam_home, data=parameters) as r:
             if is_good_response(r):
-                return (r.text)
+                if success_login(r):
+                    if raw:
+                        passreturn (r.text)
+                    else:
+                        return 0
+                else:
+                    log_error("Wrong username or password!\nError while trying to login as username: {}".format(username))
             else:
                 return None
 
     except RequestException as e:
         log_error("Error {}".format(str(e)))
+
+def success_login(r):
+    html = BeautifulSoup(r.text, 'html.parser')
+    return (html.find("input", class_="button") == None)
 
 def is_good_response(r):
     content_type = r.headers['Content-Type'].lower()
@@ -27,12 +41,12 @@ def is_good_response(r):
 def log_error(e):
     print(e)
 
-def get_bio(username, pswd):
-    return get.biodata(BeautifulSoup(login(username, pswd), 'html.parser'))
+def get_bio(username=None):
+    return get.biodata(BeautifulSoup(login(username), 'html.parser'))
 
-def get_jadwal(username, pswd):
+def get_jadwal(username=None):
     try:
-        with session.get("https://siam.ub.ac.id/class.php") as r:
+        with session.get(siam_home+"class.php") as r:
             if is_good_response(r):
                 return get.jadwal(BeautifulSoup(r.text, 'html.parser'))
             else:
@@ -40,11 +54,11 @@ def get_jadwal(username, pswd):
     except RequestException as e:
         log_error("Error {} as {}".format(str(e), username))
 
-def get_all_data(username, pswd, split=True):
+def get_all_data(username=None, split=True):
     if split:
-        return get_bio(username, pswd), get_jadwal(username, pswd)
+        return get_bio(username), get_jadwal(username)
     else:
         retr = [[]]
-        retr.append(get_bio(username, pswd))
-        retr.append(get_jadwal(username, pswd))
+        retr.append(get_bio(username))
+        retr.append(get_jadwal(username))
         return retr
